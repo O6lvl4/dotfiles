432a66bd597f602efe7e6a6de745b6e4cd2a425b
O6lvl4 <96694331+O6lvl4@users.noreply.github.com>
2022-07-24 09:24:00 +0900

    Fix typo

--- cookbooks/seeds/homebrew/default.rb
+++ cookbooks/seeds/homebrew/default.rb
@@ -1,5 +1,5 @@
 execute "setup homebrew" do
   install_url = "https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
-  command "arch -x86_64 /bin/bash -c \"$(curl -fsSL ${install_url})\""
+  command "arch -x86_64 /bin/bash -c \"$(curl -fsSL #{install_url})\""
   not_if "which brew"
 end
