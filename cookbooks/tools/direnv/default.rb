81e003f1a83e40a96ffeffea8337bb999dcd774e
O6lvl4 <o6lvl4@O6lvl4noMacBook-Air.local>
2022-08-06 17:57:43 +0900

    Update direnv recipe of not if condition

--- cookbooks/tools/direnv/default.rb
+++ cookbooks/tools/direnv/default.rb
@@ -1,8 +1,10 @@
 git File.expand_path("~/.direnv") do
   repository "https://github.com/direnv/direnv.git"
+  not_if "which direnv"
 end
 
 install_url = "https://direnv.net/install.sh"
 execute "direnv install" do
   command "curl -sfL #{install_url} | bash"
+  not_if "which direnv"
 end
